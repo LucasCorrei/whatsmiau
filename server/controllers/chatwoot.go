20355ed83353a25c1406877ae3857f566ea5a21f
LucasCorrei <lucasjb@hotmail.com>
2026-02-26 16:51:07 -0300

    Add QuoteMessage field to chatwoot payload

--- server/controllers/chatwoot.go
+++ server/controllers/chatwoot.go
@@ -194,6 +194,7 @@ func (c *Chatwoot) ReceiveWebhook(ctx echo.Context) error {
 				RemoteJID:      &jid,
 				Text:           payload.Content,
 				QuoteMessageID: quotedMessageID,
+				QuoteMessage: payload.Content,
 			})
 
 			if err != nil {
