61b5405b349e821367dd2bcbe99a4d0031dbefcf
LucasCorrei <lucasjb@hotmail.com>
2026-02-27 17:55:11 -0300

    Update instance.go

--- models/instance.go
+++ models/instance.go
@@ -1,5 +1,19 @@
 package models
 
+
+type InstanceWebhook struct {
+    Url    string   `json:"url,omitempty"`
+    Base64 *bool    `json:"base64,omitempty"`
+    Events []string `json:"events,omitempty"`
+}
+
+type InstanceProxy struct {
+    ProxyHost     string `json:"proxyHost,omitempty"`
+    ProxyPort     string `json:"proxyPort,omitempty"`
+    ProxyProtocol string `json:"proxyProtocol,omitempty"`
+    ProxyUsername string `json:"proxyUsername,omitempty"`
+    ProxyPassword string `json:"proxyPassword,omitempty"`
+}
 type Instance struct {
 	ID string `json:"id,omitempty"`
 
